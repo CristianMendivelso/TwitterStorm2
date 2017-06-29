9c870b2834c1dc17a84b9a6b657e0939d128fd45
henrymonroy7 <henry_monroy7@hotmail.com>
2017-06-29 10:02:41 -0500

    adición archivo salida

--- src/main/java/com/autentia/tutoriales/bolt/CountPrinterBolt.java
+++ src/main/java/com/autentia/tutoriales/bolt/CountPrinterBolt.java
@@ -9,6 +9,12 @@ import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Tuple;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CountPrinterBolt extends BaseRichBolt {
 
@@ -20,8 +26,19 @@ public class CountPrinterBolt extends BaseRichBolt {
 	public void execute(Tuple tuple) {
 		final String word = tuple.getStringByField("word");
 		final MutableInt count = (MutableInt) tuple.getValueByField("count");
-		
-		System.out.println(String.format("%s:%s", word, count.toString()));
+		String ruta = "/home/administrador/storm/output/archivo.txt";
+                File archivo = new File(ruta);
+                BufferedWriter bw = null;
+            try {
+                bw = new BufferedWriter(new FileWriter(archivo));
+            } catch (IOException ex) {
+                Logger.getLogger(CountPrinterBolt.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                bw.write(String.format("%s:%s", word, count.toString()));
+            } catch (IOException ex) {
+                Logger.getLogger(CountPrinterBolt.class.getName()).log(Level.SEVERE, null, ex);
+            }
 	}
 
 	@Override
