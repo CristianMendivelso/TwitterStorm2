affdbe8cae9d335bd27a060a7d232fd28456e055
cristianmendivelso <cristian.mendivelso@mail.escuelaing.edu.co>
2017-07-05 11:29:42 -0500

    prueba

--- src/main/java/com/autentia/tutoriales/WordCountTopology.java
+++ src/main/java/com/autentia/tutoriales/WordCountTopology.java
@@ -25,7 +25,7 @@ public class WordCountTopology {
 
 		final Config conf = new Config();
 		//conf.setDebug(false);
-                conf.setNumWorkers(5);
+                conf.setNumWorkers(3);
                 StormSubmitter.submitTopology("words", conf, builder.createTopology());
 
 		//final LocalCluster cluster = new LocalCluster();
